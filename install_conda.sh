d26cc01b0922f1d6a6434b18ba653a6c8826d1af
Andnet DeBoer <deboerandnet@gmail.com>
2026-05-04 13:28:54 -0500

    update installation

--- install_conda.sh
+++ install_conda.sh
@@ -5,6 +5,11 @@ apt-get update && apt-get install -y wget git build-essential
 wget -q https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O /tmp/mc.sh
 bash /tmp/mc.sh -b -p /opt/conda
 rm /tmp/mc.sh
-echo 'export PATH="/opt/conda/bin:$PATH"' >> ~/.bashrc
-source ~/.bashrc
+echo 'source /opt/conda/etc/profile.d/conda.sh' >> ~/.bashrc
+source /opt/conda/etc/profile.d/conda.sh
+conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/main
+conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/r
 conda --version
+
+# Allow git to operate on directories owned by other users (common in Docker)
+git config --global --add safe.directory '*'
